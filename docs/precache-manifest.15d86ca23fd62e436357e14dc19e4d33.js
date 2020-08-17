5c72a91a6f614615fc7d32149d5566a383096567
Rodrigo <rvaras@ceapro.cl>
2020-08-17 11:00:24 -0300

    docs agregado

---
+++ docs/precache-manifest.15d86ca23fd62e436357e14dc19e4d33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c280672dae863b8eef8d614d1296fbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b99cf09830e94fd0cc6e",
+    "url": "/static/css/main.b6cf2e08.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b99cf09830e94fd0cc6e",
+    "url": "/static/js/main.18e93ba2.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
